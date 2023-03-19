d9babd603384e787203f4323aaa4f91b5af738e4
Harshit Ramolia <ramolia.mh@iitgn.ac.in>
2023-03-20 01:21:58 +0530

    data

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,24 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { Paper } from '@mui/material'
+import { useEffect } from 'react'
+import axios from 'axios'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  useEffect(() => {
+    axios.get("data/manifest.json").then((response) => console.log(response.data))
+  }, [])
+  
   return (
     <>
+    <Paper elevation={0} sx={{}}  className="roundness" > 
+
       Harshit
+
+    </Paper>
     </>
   )
 }
